aa2302441b8fb155e371f68f7b441c36b2f4095d
shreeyagundreddy-del <shreeya.gundreddy@gmail.com>
2026-01-13 18:01:52 -0500

    Create create_schemas.sql

---
+++ sql/create_schemas.sql
@@ -0,0 +1,10 @@
+-- Author: Shreeya
+-- Owner: Data Engineering
+-- Purpose: Schema creation
+-- Layer: Governance
+-- Version: v1.0
+
+CREATE SCHEMA raw;
+CREATE SCHEMA curated;
+CREATE SCHEMA dq;
+CREATE SCHEMA audit;
