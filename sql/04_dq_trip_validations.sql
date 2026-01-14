9bebff0ba43bb28b91163bbfb2f0caf976e9a307
shreeyagundreddy-del <shreeya.gundreddy@gmail.com>
2026-01-14 01:13:52 -0500

    04_dq_trip_validations.sql

--- sql/04_dq_trip_validations.sql
+++ sql/04_dq_trip_validations.sql
@@ -3,10 +3,16 @@
 -- Layer: Data Quality
 -- Version: v1.0
 
-INSERT INTO dq.results
+INSERT INTO dq.results (
+    rule_name,
+    failed_records,
+    total_records,
+    checked_at
+)
 SELECT
     'trip_duration_positive',
+    COUNT(*) FILTER (WHERE trip_duration_min <= 0),
     COUNT(*),
     CURRENT_TIMESTAMP
-FROM curated.vw_trips_base
-WHERE trip_duration_min <= 0;
+FROM curated.vw_trips_base;
+
